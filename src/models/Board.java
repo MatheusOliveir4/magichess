03e9e49b1606cd658b691e8652407356d5ca61ec
MatheusOliveir4 <matheusfloliveira@gmail.com>
2025-05-25 16:55:24 -0300

    Board created

---
+++ src/models/Board.java
@@ -0,0 +1,86 @@
+package models;
+import models.enums.Color;
+import models.enums.PieceType;
+import models.pieces.Bisp;
+import models.pieces.King;
+import models.pieces.Knight;
+import models.pieces.Pawn;
+import models.pieces.Piece;
+import models.pieces.Queen;
+import models.pieces.Tower;
+
+public class Board {
+    public static final String TITLE_ART =
+            "##   ##    ##      ####     ####     ####    ##  ##   ######    ####     ####\n" +
+            " ### ###   ####    ##  ##     ##     ##  ##   ##  ##   ##       ##  ##   ##  ##\n" +
+            " #######  ##  ##   ##         ##     ##       ##  ##   ##       ##       ##\n" +
+            " ## # ##  ######   ## ###     ##     ##       ######   ####      ####     ####\n" +
+            " ##   ##  ##  ##   ##  ##     ##     ##       ##  ##   ##           ##       ##\n" +
+            " ##   ##  ##  ##   ##  ##     ##     ##  ##   ##  ##   ##       ##  ##   ##  ##\n" +
+            " ##   ##  ##  ##    ####     ####     ####    ##  ##   ######    ####     ####\n";
+
+
+    private static Piece[][] GRID = new Piece[8][8];
+
+    public Board() {
+        initializePieces(Color.WHITE);
+        initializePieces(Color.BLACK);
+    }
+
+    public static void initializePieces(Color color) {
+        int row = (color == Color.WHITE) ? 7 : 0;
+
+        GRID[row][0] = new Tower(row, 0, color, PieceType.TOWER);
+        GRID[row][7] = new Tower(row, 7, color, PieceType.TOWER);
+
+        GRID[row][1] = new Knight(row, 1, color, PieceType.KNIGHT);
+        GRID[row][6] = new Knight(row, 1, color, PieceType.KNIGHT);
+
+        GRID[row][2] = new Bisp(row, 2, color, PieceType.BISP);
+        GRID[row][5] = new Bisp(row, 5, color, PieceType.BISP);
+
+        GRID[row][3] = new Queen(row, 3, color, PieceType.QUEEN);
+
+        GRID[row][4] = new King(row, 4, color, PieceType.KING);
+
+        row = (color == Color.WHITE) ? 6 : 1;
+
+        for (int i = 0; i < 8; i++) {
+            GRID[row][i] = new Pawn(row, i, color, PieceType.PAWN);
+        }
+    }
+
+    public static void updateBoard() {
+        for (int x = 0; x < 8; x++) {
+            for (int y = 0; y < 8; y++) {
+                GRID[x][y] = null;
+            }
+        }
+
+        // for (Piece p : whitePlayer.getPieces()) grid[p.getPositionX()][p.getPositionY()] = p;
+        // for (Piece p : blackPlayer.getPieces()) grid[p.getPositionX()][p.getPositionY()] = p;
+    }
+
+    public static Piece getPieceFromBoard(int posX, int posY) {
+        return GRID[posX][posY];
+    }
+
+    public static void printBoard() {
+        System.out.println("\n    a  b  c  d  e  f  g  h");
+        System.out.println("   -------------------------");
+
+        for (int x = 0; x < 8; x++) {
+            System.out.print((8 - x) + " |");
+
+            for (int y = 0; y < 8; y++) {
+                Piece p = GRID[x][y];
+                System.out.print(p == null ? " . " : " " + p + " ");
+            }
+            
+            System.out.println("| " + (8 - x));
+        }
+        
+        System.out.println("   -------------------------");
+        System.out.println("    a  b  c  d  e  f  g  h");
+    }
+}
